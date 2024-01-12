b53a73d42939e36345205091d755d043d6d7851f
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-12 17:19:18 +0100

    feat: add const main.js

--- Ciclo_For - Snack 3/main.js
+++ Ciclo_For - Snack 3/main.js
@@ -2,4 +2,5 @@
 const cortoArray = [11, 12, 13, 14];
 //Costante per definire l'array lungo
 const lungoArray = [15, 16, 17, 18, 19, 20];
-
+//Costante per determinare la lunghezza minima tra i due array
+const minimoLength = Math.min(array1.length, array2.length);
