3792f5a9ae4fc43c8b5e1a88df90ce24be659b5b
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-12 17:03:10 +0100

    feat: add cicle while for the attempts

--- Ciclo_While - Snack 2/main.js
+++ Ciclo_While - Snack 2/main.js
@@ -1,5 +1,22 @@
-// Costante per generare il numero casuale
-const numeroCasuale = Math.floor(Math.random() * 100) + 1;
-
-// Variabile per tenere il conto dei tentativi
+// Variabile per tenere il conto dei tentativi fatti
 let tentativi = 0;
+
+// Ciclo per indovinare il numero dell'utente 
+while (true) {
+    // Costante per chiedere il numero all'utente
+    const tentativoUtente = parseInt(prompt("Indovina il numero tra 1 e 100"));
+
+    // Istruzione per incrementare il numero dei tentativi
+    tentativi++;
+
+    // SE l'utente indovina il numero, fine del gioco
+    if (tentativoUtente === numeroCasuale) {
+        alert(`Complimenti! Hai indovinato il numero in ${tentativi} tentativi.`);
+        break; 
+        // ALTRIMENTI l'utente riprova
+    } else if (tentativoUtente > numeroCasuale) {
+        alert("Il numero è troppo alto. Prova di nuovo!");
+    } else {
+        alert("Il numero è troppo basso. Prova di nuovo!");
+    }
+}
