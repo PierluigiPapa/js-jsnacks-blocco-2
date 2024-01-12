c556e095471f05b4e5ee3a3a775bf71d180a7a9e
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-12 18:09:19 +0100

    feat: add code js Ciclo_For - Snack 3

--- Ciclo_For - Snack 3/main.js
+++ Ciclo_For - Snack 3/main.js
@@ -2,5 +2,18 @@
 const cortoArray = [11, 12, 13, 14];
 //Costante per definire l'array lungo
 const lungoArray = [15, 16, 17, 18, 19, 20];
-//Costante per determinare la lunghezza minima tra i due array
-const minimoLength = Math.min(array1.length, array2.length);
+//Costante per determinare la lunghezza tra i due array
+const minimoLength = Math.min(cortoArray.length, lungoArray.length);
+
+// Ciclo per aggiungere elementi all'array più corto fino a quando ha lo stesso numero di elementi dell'altro
+for (let i = 0; i < minimoLength; i++) {
+    if (cortoArray.length < lungoArray.length) {
+        cortoArray.push(0); 
+    } else {
+        lungoArray.push(0);
+    }
+}
+
+// Stampo il numero degli array nella console
+console.log("Array 1:", cortoArray);
+console.log("Array 2:", lungoArray);
